d5dcf81f9ff2acb4db7b64d172104a872cc29162
Frost777777 <Andrey-vasilenko-1994@mail.ru>
2022-11-27 01:08:38 +0300

    сумма

--- RandomSum/Program.cs
+++ RandomSum/Program.cs
@@ -1,7 +1,7 @@
 ﻿int numberA = new Random().Next(1, 10); //1 2 3 4 . . . 9
-Console.WriteLine(numberA)
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
-Console.W
+Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine (result);
+Console.WriteLine(result);
 
