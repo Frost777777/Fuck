799b8fbb059ffb83a23fcccbe88e6fa082676d3c
Frost777777 <Andrey-vasilenko-1994@mail.ru>
2022-11-27 01:55:11 +0300

    алгоритм

---
+++ Приветствие/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лидия")
+{
+    Console.WriteLine("Я тебя обожаю, моя кроха!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
